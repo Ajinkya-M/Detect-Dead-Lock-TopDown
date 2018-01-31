ea090801591eb646bbf1011569e1cf08466b279c
Ajinkya Mankar <ajinkyamankar25@gmail.com>
2018-01-31 13:39:28 +0530

    Data structure updated

--- DataStructure.h
+++ DataStructure.h
@@ -16,3 +16,15 @@ typedef struct Topo_graph{
   int no_of_nodes;
   vector< vector <int> > t;
 }Topo;
+
+class GlobalState{
+
+    public:
+    vector<string> machine_states;
+	vector<int> machine_state_no;
+    vector<string> message_queue;
+    vector<int> machine_iterator;
+    int is_next_state_present;
+	int machine_no_to_explore;
+
+};
